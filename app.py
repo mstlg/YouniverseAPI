abb3a9d637b1edcf214533a029143d55743bd6ec
mstlg <madeleinestlg@gmail.com>
2018-06-11 19:37:54 +1200

    Put method started

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_restful import Resource, Api
 from bson.json_util import dumps
-from pymongo import MongoClient
+from pymongo import MongoClient, ReturnDocument
 
 import json
 
@@ -22,6 +22,18 @@ class User(Resource):
         user = user_collection.find_one({"username":username})
         json_user = json.loads(dumps(user))
         return json_user
+
+    def put(self, username):
+
+        json_user = request.get_json()
+        username_req = json_user["username"]
+        user = user_collection.find_one({"username": username_req})
+
+        updated_user = user_collection.insert_one({"username": username_req}, {"$set": json_user},
+                                                                         return_document=ReturnDocument.AFTER)
+        json_user_update = json.loads(dumps(updated_user))
+        return json_user_update
+
 
 api.add_resource(User, "/user/<username>")
 
